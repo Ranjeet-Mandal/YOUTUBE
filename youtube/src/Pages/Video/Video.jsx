bf2647d740a5bdd9faa6ada2816e6df5ddbdc016
Ranjeet Mandal <ranjeetmandal000@gmail.com>
2024-06-27 18:33:48 +0530

    commit message

---
+++ youtube/src/Pages/Video/Video.jsx
@@ -0,0 +1,16 @@
+// import Playvideo from "../../Components/Playvideo/Playvideo";
+import { useParams } from "react-router-dom";
+import Playvideo from "../../Components/Playvideo/Playvideo";
+import Recommended from "../../Components/Recommended/Recommended";
+import "./Video.css";
+const Video = ()=>{
+    const {videoId, categoryId} = useParams();
+
+    return (
+        <div className="play-container">
+            <Playvideo videoId={videoId}></Playvideo>
+            <Recommended></Recommended>
+        </div>
+    )
+}
+export default Video;
